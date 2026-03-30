7dc42a8f954ec03a7675ba62d7075a1244e28ebb
anushkaraks <anushka.anchu@gmail.com>
2026-03-30 08:26:19 +0530

    UC1: Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,3 +1,13 @@
-public class TrainConsistManagementApp {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TrainConsistManagementApp {
+public static void main(String [] args){
+    System.out.println("==========================================");
+    System.out.println("=== Train Consist Management App ===");
+    System.out.println("==========================================\n");
+
+    List<String> trainConsist= new ArrayList<>();
+    System.out.println("Train initialised successfully ....")
+}
 }
