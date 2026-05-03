e03719845f16e0a56506721b7a29fc3461d59840
Anushka Rakesh <anushka.anchu@gmail.com>
2026-05-03 21:09:23 +0530

    UC12
    Added CargoSafetyException and GoodsBogie classes to manage cargo assignments with safety checks.

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,3 +1,52 @@
-public class TrainConsistManagementApp {
+package TrainConsistencyManagementApp;
 
+class CargoSafetyException extends RuntimeException {
+    public CargoSafetyException(String message) {
+        super(message);
+    }
+}
+
+class GoodsBogie {
+    String shape;
+    String cargo;
+
+    GoodsBogie(String shape) {
+        this.shape = shape;
+    }
+
+    public void assignCargo(String cargoType) {
+        try {
+            if (shape.equalsIgnoreCase("Rectangular") && cargoType.equalsIgnoreCase("Petroleum")) {
+                throw new CargoSafetyException("Unsafe cargo assignment");
+            }
+            this.cargo = cargoType;
+            System.out.println("Cargo assigned: " + cargoType + " to " + shape + " bogie");
+        } catch (CargoSafetyException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Cargo assignment attempt completed");
+        }
+    }
+
+    public String toString() {
+        return shape + " bogie carrying " + (cargo == null ? "no cargo" : cargo);
+    }
+}
+
+public class trainConsistencyManagementApp {
+
+    public static void main(String[] args) {
+
+        GoodsBogie b1 = new GoodsBogie("Cylindrical");
+        b1.assignCargo("Petroleum");
+        System.out.println(b1);
+
+        GoodsBogie b2 = new GoodsBogie("Rectangular");
+        b2.assignCargo("Petroleum");
+        System.out.println(b2);
+
+        GoodsBogie b3 = new GoodsBogie("Rectangular");
+        b3.assignCargo("Food");
+        System.out.println(b3);
+    }
 }
